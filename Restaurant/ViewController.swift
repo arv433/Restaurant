d7710fa5cf9d822015cd21029d2519dd0994543b
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-12-02 12:16:14 -0500

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Arvin Zojaji on 2018-12-02.
+//  Copyright © 2018 Arvin Zojaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
